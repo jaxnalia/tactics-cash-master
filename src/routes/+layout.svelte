cff702802f36f9d7f15a6604b64d85d6ce961198
jaxnalia <jacksendall@gmail.com>
2024-04-15 17:51:06 +0100

    com

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,16 +8,16 @@
 
 <svelte:head>
 	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:site" content="@pls_lean">
-	<meta name="twitter:creator" content="@pls_lean">
-	<meta name="twitter:title" content="LEAN Token">
-	<meta name="twitter:description" content="Artificially scarce liquidity on PulseChain.">
-	<meta name="twitter:image" content="https://bigjax.s3.eu-west-2.amazonaws.com/twitter-image-2.jpg">
-	<meta property="og:title" content="LEAN Token">
+	<meta name="twitter:site" content="@TacticsPLS">
+	<meta name="twitter:creator" content="@TacticsPLS">
+	<meta name="twitter:title" content="TACTICS">
+	<meta name="twitter:description" content="Tactics is a DAO that incentivises a liquidity strategy based on Heart's Law to create an ecosystem bonded with the price appreciation of PulseX and Incentive Token.">
+	<!-- <meta name="twitter:image" content="https://bigjax.s3.eu-west-2.amazonaws.com/twitter-image-2.jpg"> -->
+	<meta property="og:title" content="Tactics">
 	<meta property="og:type" content="website">
-	<meta property="og:url" content="https://leantoken.org/">
-	<meta property="og:image" content="https://bigjax.s3.eu-west-2.amazonaws.com/twitter-image-2.jpg">
-	<meta property="og:description" content="Artificially scarce liquidity on PulseChain.">
+	<meta property="og:url" content="https://tactics.cash/">
+	<!-- <meta property="og:image" content="https://bigjax.s3.eu-west-2.amazonaws.com/twitter-image-2.jpg"> -->
+	<meta property="og:description" content="Tactics is a DAO that incentivises a liquidity strategy based on Heart's Law to create an ecosystem bonded with the price appreciation of PulseX and Incentive Token.">
 </svelte:head>
 <Analytics />
 
